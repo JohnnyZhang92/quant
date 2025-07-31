43334399cd595eaea85e3d05be8b0daa4797f928
bnnzhang <752066837@qq.com>
2025-07-31 18:57:45 +0800

    备注枚举值来源

--- stock_enum/stock_enum.py
+++ stock_enum/stock_enum.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 
+# https://www.csindex.com.cn/zh-CN/indices/index-detail/H30374#/indices/family/list
 class StockEnum(Enum):
     沪深300 = "000300"
     大宗商品 = "000979"
@@ -11,6 +12,5 @@ class StockEnum(Enum):
     中证500 = "000905"
     国债10年 = "930916"
     证券30 = "931412"
-
     科创创业50 = "931643"
     科创50 = "000688"
