ff26b0dd5a6d42d0f0fb700175be2b0557dd0f8b
bnnzhang <752066837@qq.com>
2025-08-02 21:10:30 +0800

    mac系统字体，价格归一化取mean

--- util/plt_normalized_price.py
+++ util/plt_normalized_price.py
@@ -4,7 +4,8 @@ from util.fetch_data import fetch_index_data
 from stock_enum.stock_enum import StockEnum
 from stock_enum.data_index_enum import DataIndexEnum
 import matplotlib
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
+# matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # windows系统显示中文
+matplotlib.rcParams['font.family'] = ['Kaiti SC']  # mac显示中文
 matplotlib.rcParams['axes.unicode_minus'] = False    # 正常显示负号
 
 def plt_normalized_price(stocks=StockEnum):
@@ -13,7 +14,7 @@ def plt_normalized_price(stocks=StockEnum):
     for name, df in data_map.items():
         close = df[[DataIndexEnum.DATE.value, DataIndexEnum.CLOSE.value]].copy()
         # 归一化
-        close[name] = close[DataIndexEnum.CLOSE.value] / close[DataIndexEnum.CLOSE.value].max()
+        close[name] = close[DataIndexEnum.CLOSE.value] / close[DataIndexEnum.CLOSE.value].mean()
         close = close[[DataIndexEnum.DATE.value, name]]
         close.set_index(DataIndexEnum.DATE.value, inplace=True)
         close_dfs.append(close)
@@ -27,5 +28,9 @@ def plt_normalized_price(stocks=StockEnum):
 
 
 if __name__ == "__main__":
-    stocks = [StockEnum.科创50, StockEnum.科创创业50]
+    stocks = [
+
+        StockEnum.医疗50,
+        StockEnum.科创50,
+    ]
     plt_normalized_price(stocks)
