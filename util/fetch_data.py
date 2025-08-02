de5c7b781c0c08300f9887075ced13dd6b712513
bnnzhang <752066837@qq.com>
2025-08-02 21:10:06 +0800

    默认取2015年开始的数据

--- util/fetch_data.py
+++ util/fetch_data.py
@@ -4,7 +4,7 @@ from stock_enum.stock_enum import StockEnum
 
 
 # 获取时需要关闭vpn
-def fetch_index_data(stocks=StockEnum, start_date="20100101", end_date=datetime.datetime.now().strftime("%Y%m%d")):
+def fetch_index_data(stocks=StockEnum, start_date="20150101", end_date=datetime.datetime.now().strftime("%Y%m%d")):
     result = {}
     for stock in stocks:
         df = ak.stock_zh_index_hist_csindex(
